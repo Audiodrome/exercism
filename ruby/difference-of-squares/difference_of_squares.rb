f0e3e95641eb7dd5cb6026cf19f0ff17f29655c4
Audiodrome <lien.daniel.h@gmail.com>
2016-03-21 22:09:49 -0700

    difference of squares

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,15 +1,37 @@
 class Squares
-
+  VERSION = 2
   def initialize (n)
     @n = n
   end
   
   def square_of_sum
-    puts @n
-    return @n
+    x = @n
+    sq_sum = 0
+    while x > 0
+      sq_sum = sq_sum + x
+      x -= 1
+    end
+    sq_sum = sq_sum**2
+    return sq_sum
+  end
+
+  def sum_of_squares
+    x = @n
+    sum_sq = 0
+    while x > 0
+      sum_sq = sum_sq + (x**2)
+      x -=1
+    end
+    return sum_sq
+  end
+
+  def difference
+    a = square_of_sum
+    b = sum_of_squares
+    a = a - b
+    return a
   end
 end
 
-sq_sum = Squares.new(225).square_of_sum
+sq_sum = Squares.new(100).difference
 
-puts sq_sum
