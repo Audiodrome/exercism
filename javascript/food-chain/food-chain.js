a1d5ba69ffb1de577737f6702cc02a91b41a1a51
Audiodrome <lien.daniel.h@gmail.com>
2016-04-16 17:11:35 -0700

    Finally finished this blasted thing.

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -7,16 +7,15 @@ var FoodChain = function(){
 
 function Verse()
 {
-  var verse_num = arguments[0],
-      verse_multiplier = arguments[1];
+  var verse_num = arguments[0];
 
-  if (verse_multiplier == undefined)
-    verse_multiplier = verse_num + 1;
-  else
-    verse_multiplier += 1;
+  // if (verse_multiplier == undefined)
+  //   verse_multiplier = verse_num + 1;
+  // else
+  //   verse_multiplier += 1;
 
-  console.log('verse_num: ' + verse_num);
-  console.log('verse_multiplier: ' + verse_multiplier);
+  // console.log('verse_num: ' + verse_num);
+  // console.log('verse_multiplier: ' + verse_multiplier);
 
 
   var animal = [ '', 'fly', 'spider', 'bird', 'cat', 'dog', 'goat', 'cow' ],
@@ -24,20 +23,47 @@ function Verse()
 
   if (verse_num == 8) {
     song = PrintEighthLine();
-    console.log(song);
+    //console.log(song);
     return song;
   } else {
-    for (var i = verse_num; i < verse_multiplier; i++) {
-      song = song.concat(PrintFirstLine(i, animal));
-      song = song.concat(PrintSecondLine(i));
-      song = song.concat(PrintLoop(i, animal));
+    //for (var i = verse_num; i < verse_multiplier; i++) {
+      song = song.concat(PrintFirstLine(verse_num, animal));
+      song = song.concat(PrintSecondLine(verse_num));
+      song = song.concat(PrintLoop(verse_num, animal));
       song = song.concat(PrintSeventhLine());
       console.log(song);
-
-    }
+    //}
     return song;
   }
+}
 
+function Verses()
+{
+  var verse_num = arguments[0],
+      verse_multiplier = arguments[1],
+      last_line = 8;
+
+  var animal = [ '', 'fly', 'spider', 'bird', 'cat', 'dog', 'goat', 'cow' ],
+      song = "";
+
+  if (verse_multiplier < 8)
+    verse_multiplier += 1;
+  
+  for (var i = verse_num; i < verse_multiplier; i++) {
+    song = song.concat(PrintFirstLine(i, animal));
+    song = song.concat(PrintSecondLine(i));
+    song = song.concat(PrintLoop(i, animal));
+    song = song.concat(PrintSeventhLine());
+    song = song.concat("\n");
+    //console.log(song);
+  }
+
+  if (last_line == arguments[1])
+    song = song.concat(PrintEighthLine() + "\n");
+
+  console.log(song);
+
+  return song;
 }
 
 function PrintFirstLine(verse_num, animal)
@@ -89,6 +115,6 @@ function PrintEighthLine()
 
 var song = new FoodChain();
 
-song.verse(1,2);
+song.verses(1,8);
 
 module.exports = FoodChain;
