66c7a0fea21f839624f7e08dcd9ceb178e7707da
Audiodrome <lien.daniel.h@gmail.com>
2017-12-15 23:20:23 -0800

    Init python

---
+++ python/hello-world/hello_world.py
@@ -0,0 +1,18 @@
+# class hello_world:
+#     def hello():
+#         print('Hello, World!');
+
+# hello_world.hello();
+
+# class Hello:
+#     def hello(self):
+#         print('Hello, World!');
+
+# hello_world = Hello();
+# hello_world.hello();
+
+# def hello_world():
+#     def hello():
+#         print('Hello, World!');
+
+# hello_world.hello();
