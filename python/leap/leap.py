075c36fbb087b024b4b6dc584f5d8793048bdf03
Audiodrome <lien.daniel.h@gmail.com>
2017-12-20 00:08:13 -0800

    Include colons for conditional statements. Returns correct booleans

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,8 +1,8 @@
 def is_leap_year(year):
-    if (year % 400 == 0)
-        return true
-    if (year % 100 != 0)
-        return false
-    if (year % 4 == 0)
-        return false
-    return false
+    if (year % 400 == 0):
+        return True
+    if (year % 100 == 0):
+        return False
+    if (year % 4 == 0):
+        return True
+    return False
