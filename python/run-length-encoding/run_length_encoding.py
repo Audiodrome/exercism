df60f235eb221d0cffa21640cc0725213a744650
Audiodrome <lien.daniel.h@gmail.com>
2018-01-09 00:09:39 -0800

    [add] decode run length

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,36 +1,52 @@
 def string_concat(encoded_str, previous_char, count):
     if (count == 1):
-        encoded_str = encoded_str + previous_char
-        print(encoded_str)
+        encoded_str += previous_char
     else:
-        encoded_str = encoded_str + str(count) + previous_char
-        print(encoded_str)
+        encoded_str += str(count) + previous_char
 
     return encoded_str
 
-def decode(string):
-    pass
+def concat_n_chars(char, number):
+    if (number == ''):
+        return char
 
+    str = ''
+    number = int(number)
+    
+    for i in range(number):
+        str += char
+    
+    return str
+
+def decode(string):
+    decoded_str = ''
+    number_str = ''
+
+    for char in string:
+        if (char.isdigit()):
+            number_str += char
+        else:
+            decoded_str += concat_n_chars(char, number_str)
+            number_str = '' # reset character count
+    
+    return decoded_str
 
 def encode(string):
     count = 1
     previous_char = ''
     encoded_str = ''
-    
+
     for char in string:
-        # print(char)
         if (char == previous_char):
-            # print('i made it')
             count += 1
-            # print(count)
         else:
             encoded_str = string_concat(encoded_str, previous_char, count)
-            count = 1
+            count = 1 # reset character count
         
         previous_char = char
-   
+    
+    # concats the last char 
     encoded_str = string_concat(encoded_str, previous_char, count)
-    print('string: ', encoded_str)
 
     return encoded_str
 
