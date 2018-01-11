1566d69748612e205b28a4eb2635ad62d0b2f66f
Audiodrome <lien.daniel.h@gmail.com>
2018-01-10 23:12:49 -0800

    [add] return the correct date for nth day, [1st, 2nd, 3rd, 4th, 5th] [todo] return the correct date for which 'last'

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -3,24 +3,36 @@ from datetime import date
 def meetup_day(year, month, day_of_the_week, which):
     monthcalendar = calendar.monthcalendar(year, month)
 
-    day_to_int = {
+    day_descriptor = {
                 'Monday': 0,
                 'Tuesday': 1,
                 'Wednesday': 2,
                 'Thursday': 3,
                 'Friday': 4,
                 'Saturday': 5,
-                'Sunday': 6 
-               }
+                'Sunday': 6,
+                '1st': 1,
+                '2nd': 2,
+                '3rd': 3,
+                '4th': 4,
+                '5th': 5
+            }
 
     # print(monthcalendar)
-
+    day = day_descriptor[day_of_the_week]
+    
     if (which == 'teenth'):
         for week in monthcalendar:
-            print(week)
-            day = day_to_int[day_of_the_week]
             if (week[day] > 12 and week[day] < 20):
-                print(date(year, month, week[day]))
                 return date(year, month, week[day])
+    
+    nth_day = day_descriptor[which]
+    
+    if (date(year, month, 1).weekday() == day_descriptor[day_of_the_week]):
+        day = calendar.Calendar(day).monthdayscalendar(year, month)[nth_day - 1][0]
+        return date(year, month, day)
+    else:
+        day = calendar.Calendar(day).monthdayscalendar(year, month)[nth_day][0]
+        return date(year, month, day)
 
-meetup_day(2013, 5, 'Monday', 'teenth')
+meetup_day(2013, 11, 'Friday', '2nd')
