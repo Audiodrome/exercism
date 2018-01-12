facf8eacfccba9e9331febbe15122bbe7b3b8bd2
Audiodrome <lien.daniel.h@gmail.com>
2018-01-11 21:54:04 -0800

    [refactor] code cleanup

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -3,20 +3,20 @@ from datetime import date
 
 def meetup_day(year, month, day_of_the_week, which):
     day_descriptor = {
-                'Monday': 0,
-                'Tuesday': 1,
-                'Wednesday': 2,
-                'Thursday': 3,
-                'Friday': 4,
-                'Saturday': 5,
-                'Sunday': 6,
-                '1st': 1,
-                '2nd': 2,
-                '3rd': 3,
-                '4th': 4,
-                '5th': 5,
-                'last': 5
-            }
+        'Monday': 0,
+        'Tuesday': 1,
+        'Wednesday': 2,
+        'Thursday': 3,
+        'Friday': 4,
+        'Saturday': 5,
+        'Sunday': 6,
+        '1st': 1,
+        '2nd': 2,
+        '3rd': 3,
+        '4th': 4,
+        '5th': 5,
+        'last': 5
+    }
 
     day = day_descriptor[day_of_the_week]
     monthcalendar = calendar.monthcalendar(year, month)
@@ -27,7 +27,6 @@ def meetup_day(year, month, day_of_the_week, which):
                 return date(year, month, week[day])
     
     nth_day = day_descriptor[which]
-
     # checks if the last day exists in the last week of the month
     # if not check previous week
     if (which == 'last'):
@@ -45,4 +44,3 @@ def meetup_day(year, month, day_of_the_week, which):
         day = calendar.Calendar(day).monthdayscalendar(year, month)[nth_day][0]
         return date(year, month, day)
 
-meetup_day(2015, 2, 'Sunday', 'last')
