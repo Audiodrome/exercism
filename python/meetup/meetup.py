1ba60f92cbc1d771b65b8504a6493843b5afc42c
Audiodrome <lien.daniel.h@gmail.com>
2018-01-11 21:40:39 -0800

    [add] returns the date for the selected last day for that month [thoughts] this solution is held by tape and bubblegum

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -1,8 +1,7 @@
 import calendar
 from datetime import date
-def meetup_day(year, month, day_of_the_week, which):
-    monthcalendar = calendar.monthcalendar(year, month)
 
+def meetup_day(year, month, day_of_the_week, which):
     day_descriptor = {
                 'Monday': 0,
                 'Tuesday': 1,
@@ -15,11 +14,12 @@ def meetup_day(year, month, day_of_the_week, which):
                 '2nd': 2,
                 '3rd': 3,
                 '4th': 4,
-                '5th': 5
+                '5th': 5,
+                'last': 5
             }
 
-    # print(monthcalendar)
     day = day_descriptor[day_of_the_week]
+    monthcalendar = calendar.monthcalendar(year, month)
     
     if (which == 'teenth'):
         for week in monthcalendar:
@@ -27,7 +27,17 @@ def meetup_day(year, month, day_of_the_week, which):
                 return date(year, month, week[day])
     
     nth_day = day_descriptor[which]
-    
+
+    # checks if the last day exists in the last week of the month
+    # if not check previous week
+    if (which == 'last'):
+        if (monthcalendar[len(monthcalendar) - 1][day] != 0):
+            day = monthcalendar[len(monthcalendar) - 1][day]  
+            return date(year, month, day)
+        else:
+            day = monthcalendar[len(monthcalendar) - 2][day]
+            return date(year, month, day)
+
     if (date(year, month, 1).weekday() == day_descriptor[day_of_the_week]):
         day = calendar.Calendar(day).monthdayscalendar(year, month)[nth_day - 1][0]
         return date(year, month, day)
@@ -35,4 +45,4 @@ def meetup_day(year, month, day_of_the_week, which):
         day = calendar.Calendar(day).monthdayscalendar(year, month)[nth_day][0]
         return date(year, month, day)
 
-meetup_day(2013, 11, 'Friday', '2nd')
+meetup_day(2015, 2, 'Sunday', 'last')
