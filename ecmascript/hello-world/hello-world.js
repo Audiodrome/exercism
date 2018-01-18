af4732a1dd9af7e2a2e204845293f1c24c62d5f3
Audiodrome <lien.daniel.h@gmail.com>
2018-01-17 20:26:45 -0800

    [add] ecmascript hello world solution [update] ignore node modules from both javascript and ecmascript directories

---
+++ ecmascript/hello-world/hello-world.js
@@ -0,0 +1,13 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+'use scrict';
+
+class HelloWorld {
+  hello() {
+    return 'Hello, World!';
+  }
+}
+
+export default HelloWorld;
