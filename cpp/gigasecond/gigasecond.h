00e4a86bfa012d59623da9a1943cf298166a5888
Audiodrome <lien.daniel.h@gmail.com>
2018-01-30 22:48:47 -0800

    [add] save point [todo] figure boost ptime [todo] code in c++ more, you're super rusty

---
+++ cpp/gigasecond/gigasecond.h
@@ -0,0 +1,13 @@
+#ifndef GIGASECOND_H
+#define GIGASECOND_H
+
+#include "boost/date_time/posix_time/posix_time.hpp"
+
+using namespace boost::posix_time;
+
+class gigasecond {
+    public:
+        static ptime advance(ptime param);
+};
+
+# endif
