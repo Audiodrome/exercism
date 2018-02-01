527daf416977919268ed91faeec6b1748ae620c2
Audiodrome <lien.daniel.h@gmail.com>
2018-01-31 23:04:17 -0800

    [update] gigasecond solution

--- cpp/gigasecond/gigasecond.cpp
+++ cpp/gigasecond/gigasecond.cpp
@@ -2,8 +2,5 @@
 #include <iostream>
 
 ptime gigasecond::advance(ptime input) {
-    std::cout<<"what is input: "<<input<<std::endl;
-    const ptime actual = time_from_string("2011-04-25 00:00:00");
-    
-    return actual;
+    return input + seconds(1000000000);
 }
