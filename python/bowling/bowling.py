0f5e05ca561c7bc6bacad13d5a273352594d667e
Audiodrome <lien.daniel.h@gmail.com>
2018-02-09 23:04:01 -0800

    I need to redo this program

--- python/bowling/bowling.py
+++ python/bowling/bowling.py
@@ -1,40 +1,46 @@
-
-
 class BowlingGame(object):
     def __init__(self):
         self.rolls = []
+        self.score = 0
+        self.frameIndex = 0
 
     def roll(self, pins):
+        if pins > 10:
+            raise ValueError("There can't possibly be more than 10 pins")
+        # if len(self.rolls) == 20:
+        #     raise IndexError("There are already 10 frames.")
+        if pins < 0:
+            raise ValueError("There can't possible be less than 0 pins")
         self.rolls.append(pins)
 
     def score(self):
-        score = 0
-        frameIndex = 0
+        # score = 0
+        # frameIndex = 0
 
         def sumOfBallsInFrame():
-            return self.rolls[frameIndex] + self.rolls[frameIndex + 1]
+            return self.rolls[self.frameIndex] + self.rolls[self.frameIndex + 1]
         
         def spareBonus():
-            return self.rolls[frameIndex + 2]
+            return self.rolls[self.frameIndex + 2]
         
         def strikeBonus():
-            return self.rolls[frameIndex + 1] + self.rolls[frameIndex + 2]
+            return self.rolls[self.frameIndex + 1] + self.rolls[self.frameIndex + 2]
 
         def isStrike():
-            return self.rolls[frameIndex] == 10
+            return self.rolls[self.frameIndex] == 10
 
         def isSpare():
-            return self.rolls[frameIndex] + self.rolls[frameIndex + 1] == 10
+            return self.rolls[self.frameIndex] + self.rolls[self.frameIndex + 1] == 10
 
         for frame in range(10):
             if isStrike():
-                score += 10 + strikeBonus()
-                frameIndex += 1
+                self.score += 10 + strikeBonus()
+                self.frameIndex += 1
             elif isSpare():
-                score += 10 + spareBonus()
-                frameIndex += 2
+                self.score += 10 + spareBonus()
+                self.frameIndex += 2
             else:
-                score += sumOfBallsInFrame()
-                frameIndex += 2
+                self.score += sumOfBallsInFrame()
+                self.frameIndex += 2
         
-        return score
+        return self.score
