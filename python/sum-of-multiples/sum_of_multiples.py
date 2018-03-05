95bfa78be17190fee0e922df867bf428a0bda7d3
Audiodrome <lien.daniel.h@gmail.com>
2018-03-04 23:54:08 -0800

    [update] python supports set comprehension, nice!

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,19 +1,13 @@
 def sum_of_multiples(limit, multiples):
     serial = [i for i in range(limit)]
-    # multiples_list = []
-    # for multiple in multiples:
-    #     for num in serial:
-    #         if num % multiple == 0:
-    #             multiples_list.append(num)
-    multiples_list = [
+
+    # multiples_list is a set, no duplicates allowed
+    # set comprehension
+    multiples_list = {
         num 
         for multiple in multiples 
             for num in serial 
                 if num % multiple == 0
-    ]
-    
-    # print(multiples_list)
+    }
 
     return sum(multiples_list)
-
-sum_of_multiples(4, [3, 5])
