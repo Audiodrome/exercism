1535fc3a2746b2caf0e500e74af9f94e1b930209
Audiodrome <lien.daniel.h@gmail.com>
2018-04-04 00:31:49 -0700

    [update] sort method sorts by alphabet order for students

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -17,16 +17,9 @@ class School(object):
             self._enrolled_students[grade] += (name, )
 
     def sort(self):
-        # print(self._enrolled_students[2].sort())
-        # print(self._enrolled_students)
+        for key in self._enrolled_students:
+            a = self._enrolled_students[key]
+            self._enrolled_students[key] = tuple(sorted(a))
+        
         tmp = list(self._enrolled_students.items())
         return sorted(tmp, key=lambda tup: tup[0])
-
-school = School('Sweet Valley High')
-
-school.add("James", 2)
-school.add("Blair", 2)
-school.add("Paul", 2)
-school.sort()
-
-print(school.sort())
